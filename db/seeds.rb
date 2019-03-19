376b0bad6c0589a95f5c0a67316d93ec83468477
isalevine <isaacdlevine@gmail.com>
2019-03-18 17:40:42 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+student1 = Student.create(first_name: "Charlie", last_name: "Kelly")
+student2 = Student.create(first_name: "Mantis", last_name: "Toboggan")
+
+class1 = SchoolClass.create(title: "Ruby 101", room_number: 101)
+class2 = SchoolClass.create(title: "Rails Magic 201", room_number: 1411)
