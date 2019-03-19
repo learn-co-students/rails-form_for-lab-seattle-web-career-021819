8f25a00ede32afd5fbbdbf8abf9788326f17a544
Kimberly Heidorn <heidorkn@gmail.com>
2019-03-19 13:28:43 -0700

    building out more stuff using active record and building out some basic links and relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :school_classes, except: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, except: :destroy
 end
